ec99d7c522e3031a202c5cc2779bb6413732a6b8
Nick Jones <nickjones9605@gmail.com>
2022-09-26 11:32:59 -0600

    Serializer for book forecast facade, added new facade that combines two old facades

--- spec/facades/book_forecast_facade_spec.rb
+++ spec/facades/book_forecast_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BooksForecastFacade do
-    it 'returns a Forecast poro', :vcr do
+    it 'returns a Forecast object', :vcr do
     forecast = BooksForecastFacade.get_book_destination_forecast('Denver, CO')
     expect(forecast).to be_a Weather
     end
