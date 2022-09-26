7b83eb23a8402d3d7888ff267a916485e088d39c
Nick Jones <nickjones9605@gmail.com>
2022-09-25 21:35:31 -0600

    Adds weather service

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+
+    it 'returns forecast JSON data for a given latitude & longitude', :vcr do
+        coords = GeocodeFacade.geocode('denver,co')
+        lat = coords.lat
+        long = coords.long
+        forecast = WeatherService.forecast(coords)
+
+        expect(forecast).to be_a Hash
+        expect(forecast).to have_key(:current)
+        expect(forecast).to have_key(:hourly)
+        expect(forecast).to have_key(:daily)
+    end
+end
