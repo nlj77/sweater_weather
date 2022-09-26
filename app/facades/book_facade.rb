71671e06d0c2665699a7b29d4553cdb92615e343
Nick Jones <nickjones9605@gmail.com>
2022-09-26 10:45:01 -0600

    Onto controller

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -1,7 +1,7 @@
 class BookFacade
+    attr_reader :num_found
     def self.get_books(location, limit)
         data = BookService.get_books(location, limit)
-        num_found = data[:numFound]
         books = data[:docs].map do |book|
             Book.new(data)
         end
