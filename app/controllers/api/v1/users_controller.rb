ff3021822ce4b66c37de8b1f037e9dd229db4350
Nick Jones <nickjones9605@gmail.com>
2022-09-27 15:46:46 -0600

    Updates controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::UsersController < ApplicationController
 
-    def create
-        new_user = User.new(user_params)
-        if new_user.save
-            render json: UserSerializer.new(new_user), status: 201
-        else
-            render json: { error: 'Email Or Password Is Incorrect' }, status: 401
+    class Api::V1::UsersController < ApplicationController
+        def create
+            if User.exists?(email: params[:email])
+                render json: { message: "email already taken" }, status: 400
+            elsif params[:password] != params[:password_confirmation]
+                render json: { message: "passwords must match" }, status: 400
+            elsif params[:email] == "" || params[:password] == "" || params[:password_confirmation] == ""
+                render json: { message: "all fields must be completed" }, status: 400
+            elsif
+                user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex(10))
+                render json: UsersSerializer.return(user), status: 201
+            end
         end
-    end
-
-    private
-    def user_params
-        params.permit(:username, :password, :password_confirmation)
-    end
 end
